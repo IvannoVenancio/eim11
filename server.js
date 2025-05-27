bde5ca62de95f98f93c9e06700af367ab06d5521
Noah Tech <noah@laptop.ao>
2025-05-27 09:45:32 +0100

    setup node server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//Rota Principal
+app.get('/',(req, res)=>{
+    res.send('Ola Mundo')
+})
+app.get('/cadastro',(req, res)=>{
+    res.send('Ola Cadastro')
+})
+
+app.listen(port, ()=>{
+    console.log(`Servidor rodando na porta ${port}`)
+})
